386236534b929b849a45f2420b5cdb7e4bb088ce
kmtz3 <klara.martinez@productboard.com>
2025-04-27 12:56:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = 3000;
 
 // Middleware to parse JSON payloads
 app.use(express.json());
